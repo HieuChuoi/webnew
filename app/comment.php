febd6cf0f719b6e7abc7ba726f119f5f444e1adf
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-25 10:23:28 +0700

    create table in comment.php

--- app/comment.php
+++ app/comment.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     protected $table = "comment";
+
+    public function news()
+    {
+        return $this->belongsTo('App\news','idnews','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\user','iduser','id');
+    }
+
 }
