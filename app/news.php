00ce8c2a556a09a21edb7165a70b7d9605585b47
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-25 10:23:59 +0700

    create table in news.php

--- app/news.php
+++ app/news.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class news extends Model
 {
     protected $table = "news";
+
+    public function types()
+    {
+        return $this->belongsTo('App\types','idtypes','id');
+    }
+
+    public function comment()
+    {
+        return $this->hasMany('App\comment','idnews','id');
+    }
+
 }
