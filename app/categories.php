8c8699140167efe6596aa1331fad88538e549df3
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-25 10:27:59 +0700

    add table in categories

--- app/categories.php
+++ app/categories.php
@@ -15,6 +15,6 @@ class categories extends Model
 
     public function news()
     {
-        return $this->hasManyThrough('App\news','App\types','idcategories','idtypes','id');
+        return $this->hasManyThrough('App\news','App\types','idcategories','idtypes','id'); 
     }
 }
