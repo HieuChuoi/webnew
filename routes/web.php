94f66a1a27bbe84e77fc9ecc17330c9399788c19
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-29 22:47:14 +0700

    create add function to categories

--- routes/web.php
+++ routes/web.php
@@ -19,45 +19,36 @@ Route::group(['prefix'=>'admin'],function(){
     Route::group(['prefix'=>'categories'],function(){
         // admin/categories/add  
         Route::get('list','categoriesController@getList');
-
         Route::get('edit','categoriesController@getEdit');
-        
         Route::get('add','categoriesController@getAdd');
+        Route::post('add','categoriesController@postAdd');    
     });
 
     Route::group(['prefix'=>'types'],function(){
         // admin/types/add  
         Route::get('list','categoriesController@getList');
-
-        Route::get('edit','categoriesController@getEdit');
-        
+        Route::get('edit','categoriesController@getEdit');        
         Route::get('add','categoriesController@getAdd');
     });
 
     Route::group(['prefix'=>'news'],function(){
         // admin/news/add  
         Route::get('list','categoriesController@getList');
-
-        Route::get('edit','categoriesController@getEdit');
-        
+        Route::get('edit','categoriesController@getEdit');        
         Route::get('add','categoriesController@getAdd');
     });
 
     Route::group(['prefix'=>'slide'],function(){
         // admin/slide/add  
         Route::get('list','categoriesController@getList');
-
-        Route::get('edit','categoriesController@getEdit');
-        
+        Route::get('edit','categoriesController@getEdit');        
         Route::get('add','categoriesController@getAdd');
     });
 
     Route::group(['prefix'=>'User'],function(){
         // admin/User/add  
         Route::get('list','categoriesController@getList');
-
-        Route::get('edit','categoriesController@getEdit');
-        
+        Route::get('edit','categoriesController@getEdit');       
         Route::get('add','categoriesController@getAdd');
     });
 });
