504b7c3df1a8cf416b3c6aa9fde172693a61100e
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-30 22:19:47 +0700

    complete CRUD for categories

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -22,15 +22,16 @@ class categoriesController extends Controller
     public function postAdd(Request $request)
     {
         $this->validate($request,
-        [
-            'name' => 'required|min:3|max:100'
-        ],
-        [
-            'name.required' => 'You have not entered a category name.',
-            'name.min' => 'Category name must have 3->100 characters',
-            'name.max' => 'Category name must have 3->100 characeters',
-        ]);
-
+            [
+                'name' => 'required|min:3|max:100|unique:categories,name'
+            ],
+            [
+                'name.unique' => 'Category name is already excist',
+                'name.required' => 'You have not entered a category name.',
+                'name.min' => 'Category name must have 3->100 characters',
+                'name.max' => 'Category name must have 3->100 characeters'
+            ]
+            );
         $categories = new categories;
         $categories->name = $request->name;
         $categories->unsigned_name = changeTitle($request->name);
@@ -38,8 +39,36 @@ class categoriesController extends Controller
         return redirect('admin/categories/add')->with('notification','Add Category Success!');
     } 
 
-    public function getEdit()
+    public function getEdit($id)
     {
+        $categories = categories::find($id);
+        return view ('admin.categories.edit',['categories'=>$categories]);
+    }
 
+    public function postEdit(Request $request,$id)
+    {
+        $categories = categories::find($id);
+        $this->validate($request,
+            [
+                'name' => 'required|unique:categories,name|min:3|max:100'
+            ],
+            [
+                'name.required' => 'You not have entered a category name',
+                'name.unique' => 'Category name is already excist',
+                'name.min' => 'Category name must have 3->100 characeters',
+                'name.max' => 'Category name must have 3->100 characeters'
+            ]
+            );
+        $categories->name = $request->name;
+        $categories->unsigned_name = changeTitle($request->name);
+        $categories->save();
+        return redirect('admin/categories/edit/'.$id)->with('notification','Edit success');
+    }
+
+    public function getDelete($id)
+    {
+        $categories = categories::find($id);
+        $categories->delete();
+        return redirect('admin/categories/list')->with('notification','Delete success');
     }
 }
