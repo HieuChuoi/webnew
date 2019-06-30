d097eb6bbf085ad182aa89acb1051a3966042ade
Hieu <nguyenminhhieu2508@gmail.com>
2019-06-30 23:17:26 +0700

    edit menu layout

--- resources/views/admin/layout/menu.blade.php
+++ resources/views/admin/layout/menu.blade.php
@@ -28,25 +28,49 @@
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-cube fa-fw"></i> Product<span class="fa arrow"></span></a>
+                            <a href="admin/types/list"><i class="fa fa-bar-chart-o fa-fw"></i> Type<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="#">List Product</a>
+                                    <a href="admin/types/list">List Type</a>
                                 </li>
                                 <li>
-                                    <a href="#">Add Product</a>
+                                    <a href="admin/types/add">Add Type</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-users fa-fw"></i> User<span class="fa arrow"></span></a>
+                            <a href="admin/news/list"><i class="fa fa-bar-chart-o fa-fw"></i> New<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="#">List User</a>
+                                    <a href="admin/news/list">List New</a>
                                 </li>
                                 <li>
-                                    <a href="#">Add User</a>
+                                    <a href="admin/news/add">Add New</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="admin/slide/list"><i class="fa fa-bar-chart-o fa-fw"></i> Slide<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="admin/slide/list">List Slide</a>
+                                </li>
+                                <li>
+                                    <a href="admin/slide/add">Add Slide</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="admin/User/list"><i class="fa fa-users fa-fw"></i> User<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="admin/User/list">List User</a>
+                                </li>
+                                <li>
+                                    <a href="admin/User/list">Add User</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
